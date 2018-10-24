faaf9bd884d522a0fbfb3d35ce1290cb3af53f3f
gretchypr <gretchen.bonilla@upr.edu>
2018-10-23 23:53:05 -0400

    Exam finished

--- spark/p3a.py
+++ spark/p3a.py
@@ -1,1 +1,9 @@
 #Code here
+
+schooldf = spark.read.csv('/input/escuelasPR.csv')
+
+studentsdf = spark.read.csv('/input/studentsPR.csv')
+
+maleStudents = studentsdf.join(schooldf, (schooldf._c3 == studentsdf._c2)).filter(schooldf._c5 == 'Superior').filter((schooldf._c2 == 'Ponce') | (schooldf._c2 == 'San Juan')).filter(studentsdf._c5 == 'M').select(studentsdf._c6)
+
+maleStudents.show()
--- spark/p3b.py
+++ spark/p3b.py
@@ -1,1 +1,7 @@
 #Code here
+
+schooldf = spark.read.csv('/input/escuelasPR.csv')
+
+schooldf.createOrReplaceTempView("school")
+
+schools = spark.sql("select _c1 as District, _c2 as City, count(*) from school where _c0 == 'Arecibo' group by District, City ")
